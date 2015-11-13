6d2c17fe904fb7fa45f5f4df4f99dd38686c0b95
Claire Keum <ayoung.keum@students.olin.edu>
2015-11-12 23:16:45 -0500

    debugged added newfiles

--- proj-server.py
+++ proj-server.py
@@ -1,17 +1,11 @@
-#
-# A simple server for mortality data
-#
-# Requires the bottle framework
-# (sufficient to have bottle.py in the same folder)
-#
+
 # run with:
 #
-#    python mortality-server-2.py 8080
+#    python proj-server.py 8080
 #
 # You can use any legal port number instead of 8080
 # of course
 #
-
 
 from bottle import get, post, run, request, static_file, redirect
 import os
@@ -27,29 +21,26 @@ from json import dumps
 MORTALITYDB = "mortality.db"
 
 
-
 def pullData ():
     conn = sqlite3.connect(MORTALITYDB)
     cur = conn.cursor()
 
     try: 
-        cur.execute("""SELECT year, Cause_Recode_39, sex, education, SUM(1) as total 
+        cur.execute("""SELECT year, Cause_Recode_39, sex, SUM(1) as total 
                        FROM mortality
-                       GROUP BY year, Cause_Recode_39, sex, education""")
+                       GROUP BY year, Cause_Recode_39, sex""")
         data = [{"year":int(year),
                  "cause":int(cause),
                  "gender":sex,
-                 "education":education,
-                 "total":total} for (year, cause, sex, education, total,) in  cur.fetchall()]
+                 "total":total} for (year, cause, sex, total,) in  cur.fetchall()]
         conn.close()
 
         causes = list(set([int(r["cause"]) for r in data]))
         genders = list(set([r["gender"] for r in data]))
-        education = list(set([r["education"] for r in data]))
+
         return {"data":data, 
                 "genders":genders,
-                "causes":causes,
-                "education":education}
+                "causes":causes}
 
     except: 
         print "ERROR!!!"
